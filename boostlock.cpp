79694fc753b055dc470368fd7b62ef226f8ba810
lpy <1106111671@qq.com>
2019-10-12 17:53:53 +0800

    boost lock

--- boostlock.cpp
+++ boostlock.cpp
@@ -8,10 +8,27 @@
 #include<iostream>
 #include<boost/thread.hpp>
 using namespace std;
+static boost::shared_mutex rwMutext;
+typedef boost::shared_lock<boost::shared_mutex> ReadLock;
+typedef boost::unique_lock<boost::shared_mutex> WriteLock;
+int g_i = 0;
+
+void fun()
+{
+	for (int i = 0; i < 100; i++)
+	{
+		WriteLock lock(rwMutext);
+		cout << g_i++ << endl;
+	}
+}
 
 int main(int argc, char** argv)
 {
-	
+	boost::thread t1(fun);	
+	boost::thread t2(fun);	
+
+	t1.join();
+	t2.join();
 	return 0;
 }
 
