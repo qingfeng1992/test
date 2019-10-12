e0900d28210bcaed47d05ca99e79cfda7aad9913
lpy <1106111671@qq.com>
2019-10-12 17:43:08 +0800

    boost lock test

---
+++ boostlock.cpp
@@ -0,0 +1,17 @@
+/*************************************************************************
+    > File Name: boostlock.cpp
+    > Author: ma6174
+    > Mail: ma6174@163.com 
+    > Created Time: 2019年10月12日 星期六 17时38分36秒
+ ************************************************************************/
+
+#include<iostream>
+#include<boost/thread.hpp>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	
+	return 0;
+}
+
