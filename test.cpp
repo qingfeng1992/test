9d34f0aa8f973a875e8779e16b49ba4f12a732e9
lpy <1106111671@qq.com>
2019-11-19 17:02:43 +0800

    test

--- test.cpp
+++ test.cpp
@@ -6,9 +6,85 @@
  ************************************************************************/
 
 #include<iostream>
+#include<set>
+#include<vector>
 using namespace std;
+
+class A
+{
+	public:
+		A()
+		{
+			cout << "A construct " << ++nObjCount << endl;
+		}
+		A(const A &other)
+		{
+			cout << "copy construct " << ++nObjCount << endl;	
+			i = other.i;
+			d = other.d;
+		}
+		A& operator = (const A &other)
+		{
+			cout << "A operator =" << endl;	
+
+			//赋值函数一定要检查自赋值
+			if (this != &other)
+			{
+				i = other.i;
+				d = other.d;
+			}
+			return *this;
+		}
+
+		~A()
+		{
+			cout << "A destruct" << endl;
+		}
+	public:
+		bool operator < (const A &other) const
+		{
+			return i < other.i;	
+		}
+	public:
+		int i;
+		double d;
+		static int nObjCount;
+		
+};
+
+int A::nObjCount = 0;
+
+void AddElement(set<A> &s)
+{
+	A a;
+	A a1;
+	a1.i = -1;
+	a.i = 9;
+	s.insert(a);
+	s.insert(a1);
+}
+
+void AddElement(vector<A> &v)
+{
+	A a;
+	A a1;
+	a1.i = -1;
+	a.i = 9;
+	v.push_back(a);
+	v.push_back(a1);
+}
 
 int main()
 {
+	{
+	//set<A> s;
+	vector<A> v;
+	AddElement(v);
+	for (auto &item : v)
+	{
+		cout << item.i << " ";
+	}
+	}
+
 	return 0;
 }
