819de2d11b5881fcd80c57f7cb654c5cb0535571
lpy <1106111671@qq.com>
2020-01-10 23:18:54 +0800

    ..

--- find_sub_set.cpp
+++ find_sub_set.cpp
@@ -6,13 +6,61 @@
  ************************************************************************/
 
 #include<iostream>
+#include<vector>
+#include<string>
+#include<memory>
+#include<math.h>
 using namespace std;
+typedef std::shared_ptr<vector<vector<int> > > TDArrPtr;
 
-
+TDArrPtr GetSubSet(vector<int> &vt)
+{
+	TDArrPtr ret(new vector<vector<int> >);
+	if (vt.size() > sizeof(int))
+	{
+		return ret;
+	}
+	unsigned int nCount = pow(2, vt.size());
+	for (unsigned int i = 0; i < nCount; i++)
+	{
+		vector<int> subSet;
+		//检查对应的二进制位，为1的位表示集合中需要加入对应位置的数
+		for (int j = 0; j < (int)vt.size(); j++)
+		{
+			unsigned int temp = (1 << j) & i;
+			if (0 != temp)
+			{
+				subSet.push_back(vt[j]);
+			}
+		}
+		ret->push_back(subSet);
+	}
+	return ret;
+}
 
 int main(int argc, char** argv)
 {
+	vector<int> vt;
+	vt.push_back(1);
+	vt.push_back(2);
+	vt.push_back(3);
+	TDArrPtr allSubSet = GetSubSet(vt);
+	for (auto &arr : *allSubSet)
+	{
+		cout << "{";
+		for (unsigned int k = 0; k < arr.size(); k++)
+		{
+			cout << arr[k];
+			if (k != arr.size() - 1)
+			{
+				cout << ",";
+			}
+		}
+		cout << "}";
+		cout << endl;
+	}
 
+	cout << endl;
 	return 0;
 }
 
