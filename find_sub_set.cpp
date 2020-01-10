1920ceab04fde1ded1f9a8bc28ea944b9a6a52de
lpy <1106111671@qq.com>
2020-01-11 00:08:44 +0800

    子集

--- find_sub_set.cpp
+++ find_sub_set.cpp
@@ -38,13 +38,33 @@ TDArrPtr GetSubSet(vector<int> &vt)
 	return ret;
 }
 
+TDArrPtr GetSubSet1(vector<int> &vt)
+{
+	TDArrPtr ret(new vector<vector<int> >);
+	vector<int> subSet;
+	ret->push_back(subSet);
+	for (unsigned int i = 0; i < vt.size(); i++)
+	{
+		unsigned int nCount = ret->size();
+		for (unsigned int j = 0; j < nCount; j++)
+		{
+			ret->push_back((*ret)[j]);
+			(*ret)[ret->size() - 1].push_back(vt[i]);
+		}
+	}
+
+	return ret;
+}
+
+
 int main(int argc, char** argv)
 {
 	vector<int> vt;
 	vt.push_back(1);
 	vt.push_back(2);
 	vt.push_back(3);
-	TDArrPtr allSubSet = GetSubSet(vt);
+	vt.push_back(4);
+	TDArrPtr allSubSet = GetSubSet1(vt);
 	for (auto &arr : *allSubSet)
 	{
 		cout << "{";
